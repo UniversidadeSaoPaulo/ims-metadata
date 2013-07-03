e97b47e0605b708f352aa6ebb7aee1cba00b6b74
geiser <geiser.gcc@gmail.com>
2013-07-03 18:05:19 -0300

    add readme.txt for publish in wordpress site

--- imsmd.php
+++ imsmd.php
@@ -1,12 +1,33 @@
 <?php
 /*
 Plugin Name: IMS Metadata
-Plugin URI: http://ime.usp.br/~geiser/code/wordpress/ims-metadata
-Version: 0.21
+Plugin URI: https://github.com/atp/ims-metadata
+Version: 0.8
 Description: Add IMS-metadata into post and media resource, this plugin is used to convert WP into a Learning Object Repository. The OAI-PMH specification is used to harvest the metadata through URI: HOME_URL/wp-content/plugins/ims-metadata/oai2.php?verb=Identify.
 Author: geiser.gcc
-Author URI: http://www.ime.usp.br/~geiser/
+Author URI: https://github.com/geiser
 */
+
+/*
+GNU General Public License version 3
+
+Copyright (c) 2011-2013 Marcel Bokhorst
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 require_once('config.php');
 
 class IMSMetadata {
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== ims-metadata ===
+Contributors: geiser
+Donate link: 
+Tags: ims-metadata, metadata, oai-pmh, ims-md
+Requires at least: 3.0
+Tested up to: 3.5.1
+Stable tag: 0.8
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Wordpress plugin that add IMS-metadata into post and media resource. The plugin converts WP into a Learning Object Repository. The OAI-PMH specification is used to harvest the metadata through URI: HOME_URL/wp-content/plugins/ims-metadata/oai2.php?verb=Identify.
+
+== Description ==
+
+Wordpress plugin that add IMS-metadata into post and media resource. The plugin converts WP into a Learning Object Repository. The OAI-PMH specification is used to harvest the metadata through URI: HOME_URL/wp-content/plugins/ims-metadata/oai2.php?verb=Identify.
+
+== Installation ==
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `<?php do_action('plugin_name_hook'); ?>` in your templates
+
+== Frequently asked questions ==
+
+= A question that someone might have =
+
+An answer to that question.
+
+== Screenshots ==
+
+1. 
+2. 
+
+== Changelog ==
+
+
+
+== Upgrade notice ==
+
+
+
+== Arbitrary section 1 ==
+
+
+
